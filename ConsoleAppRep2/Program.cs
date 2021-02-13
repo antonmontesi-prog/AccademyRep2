c48e2ecd4e1642189174094d0f28f8a14b4725be
antonmontesi-prog <78961830+antonmontesi-prog@users.noreply.github.com>
2021-02-13 10:52:41 +0100

    third bug
    third bug

--- ConsoleAppRep2/Program.cs
+++ ConsoleAppRep2/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppRep2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fix a second bug");
+            Console.WriteLine("Fix a third bug");
         }
     }
 }
