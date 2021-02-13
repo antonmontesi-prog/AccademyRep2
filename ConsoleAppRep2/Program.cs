f3f2c2815c577c74d324f0676f228bdd5659d389
anton montesi <anton.montesi@avanade.com>
2021-02-13 10:03:07 +0100

    Add Solution

---
+++ ConsoleAppRep2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppRep2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
