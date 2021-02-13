b6e5bc9c435a52bfe2d4370974e42f629b459688
anton montesi <anton.montesi@avanade.com>
2021-02-13 10:03:40 +0100

    ddd

--- ConsoleAppRep2/Program.cs
+++ ConsoleAppRep2/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppRep2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
